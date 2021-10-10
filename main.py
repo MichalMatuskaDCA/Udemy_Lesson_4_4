7c9376d12d032820a9b87c3e3e60f226f27bca17
Michal Matuška <88484448+MichalMatuskaDCA@users.noreply.github.com>
2021-10-10 17:35:35 +0000

    Bad Input

--- main.py
+++ main.py
@@ -28,14 +28,17 @@ scissors = '''
 
 print("What do you want to choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.")
 choice = int(input())
-choice_list = [rock, paper, scissors]
-pc_choice = random.randint(0, 2)
-print(f'Your choice :\n{choice_list[choice]}')
-print(f'PC choice :\n{choice_list[pc_choice]}')
-
-if choice == pc_choice:
-  print('Advice')
-elif choice==0 and pc_choice==2 or choice==1 and pc_choice==0 or choice==2 and pc_choice==1:
-  print('You WIN!!!')
+if choice > 2:
+  print('Type error. You entered bad number')
 else:
-  print('You LOSE!!!')
+  choice_list = [rock, paper, scissors]
+  pc_choice = random.randint(0, 2)
+  print(f'Your choice :\n{choice_list[choice]}')
+  print(f'PC choice :\n{choice_list[pc_choice]}')
+
+  if choice == pc_choice:
+    print('Game Draw')
+  elif choice==0 and pc_choice==2 or choice==1 and pc_choice==0 or choice==2 and pc_choice==1:
+    print('You WIN!!!')
+  else:
+    print('You LOSE!!!')
